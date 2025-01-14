867c71c07954768fd26fdf639a3042e1e734ca3f
泫汐 <88391974+Harry2336@users.noreply.github.com>
2025-01-14 17:52:39 +0800

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const config = {
       unionId: "oHY-uwaNsLMtx-rz7rS_MW6Hs-2I", //微信小程序抓包unionId(可选)
       sign: false, //是否自动签到
       reSign: false, //是否重新签到
-      location: "", //经纬度 (可选),例如 "120.210792,30.246026"、"经度,纬度",不填写则自动获取（推荐）
+      location: "119.9311102322481,32.463241838211786", //经纬度 (可选),例如 "120.210792,30.246026"、"经度,纬度",不填写则自动获取（推荐）
       //signImagePath: "./images/2.jpeg", //签到图片
       //needReport: true, //是否自动填写周报
     },
