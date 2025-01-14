2cffd281e21388f74b1abc52031885edbadc0f1a
泫汐 <88391974+Harry2336@users.noreply.github.com>
2025-01-14 16:29:07 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ const config = {
       openId: "ooru94sJCMm1GnlmKYBsVBbEGVVs", //微信小程序抓包openid(可选)
       unionId: "oHY-uwaNsLMtx-rz7rS_MW6Hs-2I", //微信小程序抓包unionId(可选)
       sign: true, //是否自动签到
-      reSign: true, //是否重新签到
+      reSign: false, //是否重新签到
       location: "", //经纬度 (可选),例如 "120.210792,30.246026"、"经度,纬度",不填写则自动获取（推荐）
-      signImagePath: "./images/2.jpg", //签到图片
-      needReport: true, //是否自动填写周报
+      //signImagePath: "./images/2.jpg", //签到图片
+      //needReport: true, //是否自动填写周报
     },
     // 添加多个账户
     // {
